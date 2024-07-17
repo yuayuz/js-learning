2b931d5226364d4341107f921dd71ce1c9b77120
yu <2665764951@qq.com>
2024-07-17 20:06:27 +0800

    add js-3-6

---
+++ js-3-6/src/main.js
@@ -0,0 +1,25 @@
+//for-in(枚举对象中非符号键属性)
+for(let i in [1,2,3]){
+    console.log(i);
+}
+
+//for-of（枚举对象中元素）
+for(let i of [1,2,3]){
+    console.log(i);
+}
+
+//标签语句
+start :for(let i in [1,2,3]){
+    console.log(i);
+}
+//with
+let o={
+    name : "John",
+    age : 18,
+    birth : "January",
+}
+with (o) {
+    console.log(name);
+    console.log(age);
+    console.log(birth);
+}
