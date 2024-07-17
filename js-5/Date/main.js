f629fe592793032428597709cd145c538794063c
yu <2665764951@qq.com>
2024-07-17 20:06:47 +0800

    add js-5

---
+++ js-5/Date/main.js
@@ -0,0 +1,22 @@
+let now =new Date("2024-07-13T08:00:57.657Z");
+console.log(`${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`);//2024-7-13
+console.log(now)
+
+let date1=new Date(Date.parse("2014-07-12"));
+console.log(date1)
+
+let date2=new Date(Date.UTC(2014, 0,1));//0起点月数
+console.log(date2)
+
+//toLocaleString
+console.log(now.toLocaleString()) //2024/7/13 16:00:57
+//toString
+console.log(now.toString()) //Sat Jul 13 2024 16:00:57 GMT+0800 (中国标准时间)
+//valueOf()部返回字符串，返回日期毫秒表示
+console.log(now.valueOf())
+
+console.log(now.toDateString())//Sat Jul 13 2024
+console.log(now.toTimeString())//16:00:57 GMT+0800 (中国标准时间)
+console.log(now.toLocaleDateString())//2024/7/13
+console.log(now.toLocaleTimeString())//16:00:57
+console.log(now.toUTCString())//Sat, 13 Jul 2024 08:00:57 GMT
